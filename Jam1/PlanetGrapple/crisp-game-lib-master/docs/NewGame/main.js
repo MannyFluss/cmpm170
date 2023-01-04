09d35d56c9c615380388cc6d709d892f56ab4ad3
Manny-OnGit <manny.fluss@gmail.com>
2023-01-03 16:58:49 -0800

    update

--- Jam1/PlanetGrapple/crisp-game-lib-master/docs/NewGame/main.js
+++ Jam1/PlanetGrapple/crisp-game-lib-master/docs/NewGame/main.js
@@ -66,6 +66,32 @@ b  Bb
     ,
     ];
 
+var updateList = [];
+//class declarations
+class LightningBolt
+{
+  
+  constructor(_target = vec(0,0))
+  {
+    this.target = _target;
+    this.targetAngle = Math.atan2(_target.y-G.HEIGHT/2, _target.x-G.WIDTH/2 )
+    this.targetAngleVector = vec(Math.cos(this.targetAngle),Math.sin(this.targetAngle))
+    updateList.push(this)//global list to update
+    this.position = vec(G.WIDTH/2,G.HEIGHT/2);
+    console.log(this.targetAngle)
+  }
+  update()
+  {
+    color('black');
+    char('e',this.position,{scale : vec(3,3)})
+    this.position.x += this.targetAngleVector.x;
+    this.position.y += this.targetAngleVector.y;
+    
+  }
+};
+
+
+
 
 //global variables
 var mousePosition = vec(0,0);
@@ -73,7 +99,9 @@ var mousePosition = vec(0,0);
 
 function start()
 {
-
+  mousePosition = vec(input.pos.x,input.pos.y)
+  var test = new LightningBolt(mousePosition);
+  console.log(updateList)
 }
 
 function update()
@@ -87,8 +115,30 @@ function update()
     char('b',G.WIDTH/2,G.HEIGHT/2+25,{scale : vec(3,3)})
     char('c',G.WIDTH/2,G.HEIGHT/2,{scale : vec(3,3)})
     char('d',G.WIDTH/2,G.HEIGHT/2 - 25,{scale : vec(3,3)})
-    char('e',G.WIDTH/2,G.HEIGHT/2 - 50,{scale : vec(3,3)})
+    //char('e',G.WIDTH/2,G.HEIGHT/2 - 50,{scale : vec(3,3)})
     objectsUpdate()
+
+    handleClicks();
+
+    updateList.forEach(element => {
+      element.update();
+    });
+
+}
+
+function handleClicks()
+{
+  if (input.isJustPressed)
+  {
+    fireProjectiles()
+    new LightningBolt(mousePosition)
+  }
+}
+
+function fireProjectiles()
+{   //this will execute all of the attacks in the list
+  
+
 }
 
 function objectsUpdate()
